6ad8c3186499dbacdec9e444e332655e49a6f7a2
Fengyzh <wz345@drexel.edu>
2022-02-15 15:22:50 -0500

    test

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Cryptogram </h1>
+      <h3>Just a test for git </h3>
+    </div>
+  );
+}
+
+export default App;
