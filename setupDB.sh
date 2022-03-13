dfaa85bda2d68b828775455b8042f72ce6476dcb
kb3635 <99763486+kb3635@users.noreply.github.com>
2022-03-12 21:50:42 -0500

    Add files via upload

---
+++ setupDB.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your postgres password: "
+read -s password
+psql -f createDB.sql "user=postgres password=$password" 
+echo "Database created successfully, reconnecting for table creation."
+psql -f createTable.sql "user=postgres password=$password dbname=cs375db"
+echo "Table successfully has been setup."
+
